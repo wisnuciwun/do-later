7ef670d1970c026d7aae834d9c8a46ceb106aef9
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2024-01-06 01:53:48 +0700

    feat: styling

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -25,35 +25,51 @@ const Home = () => {
 
   return (
     <div>
-      <Container fluid>
+      <div className='d-flex justify-content-between align-items-center mb-2 text-white'>
+        <div className='d-block text-start w-50'>
+          <h2><b className='text-white mb-3'>Do Later App</b></h2>
+          <span style={{ fontSize: '12px' }} className=''>
+            You can write all your todo here. But we don't responsible for your late progress,
+            because we dont have alarm to notify you 😁
+          </span>
+        </div>
+        <img style={{ width: '15%' }} src="/img-logo.png" alt="" />
+      </div>
+      <Container fluid className='border rounded p-2'>
         <Tabs
-          defaultActiveKey="profile"
+          defaultActiveKey="todo-lists"
           id="uncontrolled-tab-example"
           className="mb-3"
+          fill
         >
           <Tab eventKey="todo-lists" className='text-light w-100' title="Todos">
-            <div style={{ height: '450px', width: '500px' }}>
+            <div>
               <Accordion defaultActiveKey={0}>
                 {
-                  todos.map((v, index) => {
-                    return (
-                      <Accordion.Item className='mb-2' eventKey={index}>
-                        <Accordion.Header>{v.title}</Accordion.Header>
-                        <Accordion.Body className='text-left'>{v.description}</Accordion.Body>
-                      </Accordion.Item>
-                    )
-                  })
+                  todos.length != 0 ?
+                    todos.map((v, index) => {
+                      return (
+                        <Accordion.Item className='mb-2' eventKey={index}>
+                          <Accordion.Header>{v.title}</Accordion.Header>
+                          <Accordion.Body className='text-left'>{v.description}</Accordion.Body>
+                        </Accordion.Item>
+                      )
+                    })
+                    :
+                    <div style={{ height: '400px' }} className='w-100 d-flex justify-content-center align-items-center'>
+                      <img src="https://meeting.iconpln.co.id/resources/images/notfound2.png" alt="" />
+                    </div>
                 }
               </Accordion>
             </div>
           </Tab>
           <Tab style={{ color: 'white' }} eventKey="new-todo" title="Create new">
-            <div style={{ height: '450px', width: '500px' }}>
+            <div>
               <form onSubmit={handleClick}>
                 <div className='text-right'>
-                  <FormControl onChange={(e) => settitle(e.target.value)} style={{ width: "500px" }} className='mb-1' placeholder='Write your task title' />
-                  <FormControl onChange={(e) => setdescription(e.target.value)} className='mb-3' as='textarea' style={{ width: "500px", height: '300px' }} placeholder='Tell your task description' />
-                  <Button type="submit" className='w-100'>Save todo</Button>
+                  <FormControl required onChange={(e) => settitle(e.target.value)} className='mb-2' placeholder='Write your task title' />
+                  <FormControl required onChange={(e) => setdescription(e.target.value)} className='mb-3' as='textarea' style={{ height: '300px' }} placeholder='Tell your task description' />
+                  <Button type="submit" className='w-100 mb-1'>Save todo</Button>
                 </div>
               </form>
             </div>
