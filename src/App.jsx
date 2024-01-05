483d4f2f9cc6ac8bf3f6f3c47b513c05713a4d7f
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2024-01-05 19:07:28 +0700

    feat: initial project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <span className="material-symbols-outlined">
+          done
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default App
